e605b3f91b25df2baab21fe95fd7f8a9bdc4be50
stella505 <35947875+stella505@users.noreply.github.com>
2018-05-16 01:13:59 -0400

    Update main.go

--- ipfs-swarm-key-gen/main.go
+++ ipfs-swarm-key-gen/main.go
@@ -7,13 +7,12 @@ import "log"
 
 func main() {
 	key := make([]byte, 32)
-	key2 := make([]byte, 32)
-	_, err := rand.Read(key+key2)
+	_, err := rand.Read(key)
 	if err != nil {
 		log.Fatalln("While trying to read random source:", err)
 	}
 
 	fmt.Println("/key/swarm/psk/1.0.0/")
 	fmt.Println("/base16/")
-	fmt.Print(hex.EncodeToString(key+key2))
+	fmt.Print(hex.EncodeToString(key))
 }
